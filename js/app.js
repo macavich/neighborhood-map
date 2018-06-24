e11f3ace35e3fd601bccbc00f58f94447e6abbd9
macavich <jdealtrey@gmail.com>
2018-06-24 16:08:39 -0400

    Marker placement and zoom to new location

--- js/app.js
+++ js/app.js
@@ -12,9 +12,24 @@ function initMap() {
 
   bounds = new google.maps.LatLngBounds();
 
+  var zoomAutocomplete = new google.maps.places.Autocomplete(
+      document.getElementById('zoom-to-area-text-of-place'));
+  // Bias the boundaries within the map for the zoom to area text.
+  zoomAutocomplete.bindTo('bounds', map);
+
+  var timeAutocomplete = new google.maps.places.Autocomplete(
+      document.getElementById('zoom-to-area-text'));
+
+  // Zoom to a new area
   document.getElementById('zoom-to-area').addEventListener('click', function() {
-    findPlaces();
+    zoomToArea();
   });
+
+  // Query an area for places by category
+  document.getElementById('query-an-area').addEventListener('click', function() {
+    textSearchPlaces();
+  });
+
   // These are the real estate listings that will be shown to the user.
   // Normally we'd have these in a database instead.
   // var locations = [
@@ -54,54 +69,53 @@ function initMap() {
 function findPlaces() {
   var numItems = document.getElementById('zoom-to-area-number-of-items').value
   var placeToSearch = document.getElementById('zoom-to-area-text').value
-  console.log(numItems);
-  console.log(placeToSearch);
 
   var request = {
     query: placeToSearch,
+    bounds: map.getBounds(),
     fields: ['formatted_address', 'geometry','name']
   };
 
   var service = new google.maps.places.PlacesService(map);
   service.findPlaceFromQuery(request, function (results, status) {
     if (status == google.maps.places.PlacesServiceStatus.OK) {
-      console.log(results);
       map.setCenter(results[0].geometry.location);
       map.setZoom(8);
       map.fitBounds();
       for (var i = 0; i < Math.max(results.length, numItems); i++) {
         var place = results[i];
-        console.log(place);
         buildMarker(place, i);
       };
     } else {
-      console.log(request);
-      console.log(results);
-      console.log(status);
       return function () {
         window.alert("We couldn't find the place you were looking for!");
       }
     };
   });
 };
-
-function buildMarker(data, i) {
+function hideMarkers(markers) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(null);
+  }
+}
+function buildMarker(data) {
   console.log(data);
-  var marker_id = i;
-  var marker = new google.maps.Marker({
-    map: map,
-    position: data.geometry.location,
-    title: data.title,
-    animation: google.maps.Animation.DROP,
-    id: marker_id
-  });
+  for (var i = 0; i < data.length; i++) {
+    var marker = new google.maps.Marker({
+      map: map,
+      position: data.geometry.location,
+      title: data.title,
+      animation: google.maps.Animation.DROP,
+      id: data.id
+    });
 
-  markers.push(marker);
+    markers.push(marker);
 
-  var largeInfowindow = new google.maps.InfoWindow();
-  marker.addListener('click', function() {
-    populateInfoWindow(this, largeInfowindow);
-  });
+    var largeInfowindow = new google.maps.InfoWindow();
+    marker.addListener('click', function() {
+      populateInfoWindow(this, largeInfowindow);
+    });
+  }
 
   bounds.extend(marker.position);
   map.fitBounds(bounds);
@@ -126,6 +140,7 @@ function zoomToArea() {
   var geocoder = new google.maps.Geocoder();
   // Get the address or place that the user entered.
   var address = document.getElementById('zoom-to-area-text').value;
+  hideMarkers(markers);
   // Make sure the address isn't blank.
   if (address == '') {
     window.alert('You must enter an area, or address.');
@@ -133,13 +148,13 @@ function zoomToArea() {
     // Geocode the address/area entered to get the center. Then, center the map
     // on it and zoom in
     geocoder.geocode(
-      { address: address,
-        componentRestrictions: {locality: 'New York'}
+      { address: address
       },
       function(results, status) {
         if (status == google.maps.GeocoderStatus.OK) {
+          console.log(results);
           map.setCenter(results[0].geometry.location);
-          map.setZoom(15);
+          map.setZoom(14);
         } else {
           window.alert('We could not find that location - try entering a more' +
               ' specific place.');
@@ -147,4 +162,98 @@ function zoomToArea() {
       }
     );
   }
+}
+function textSearchPlaces() {
+  var bounds = map.getBounds();
+  hideMarkers(markers);
+  var placesService = new google.maps.places.PlacesService(map);
+  categoryToSearch = document.getElementById('zoom-to-area-text-of-place').value;
+  placesService.textSearch({
+    query: categoryToSearch,
+    bounds: bounds
+  }, function(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      limit = document.getElementById('zoom-to-area-number-of-items').value;
+      createMarkersForPlaces(results, limit);
+    }
+  });
+}
+function createMarkersForPlaces(places, limit) {
+  var bounds = map.getBounds();
+  var placelimit = (!limit) ? 7 : limit;
+  for (var i = 0; i < Math.min(places.length, placelimit); i++) {
+    var place = places[i];
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location,
+      title: place.title,
+      animation: google.maps.Animation.DROP,
+      id: place.place_id
+    });
+
+    markers.push(marker);
+
+    var placeInfoWindow = new google.maps.InfoWindow();
+    marker.addListener('click', function() {
+      getPlacesDetails(this, placeInfoWindow);
+    });
+    // bounds.extend(place.geometry.location);
+    if (place.geometry.viewport) {
+      // Only geocodes have viewport.
+      bounds.union(place.geometry.viewport);
+    } else {
+      bounds.extend(place.geometry.location);
+    }
+  }
+
+  map.fitBounds(bounds);
+}
+
+// This is the PLACE DETAILS search - it's the most detailed so it's only
+// executed when a marker is selected, indicating the user wants more
+// details about that place.
+function getPlacesDetails(marker, infowindow) {
+  var service = new google.maps.places.PlacesService(map);
+  service.getDetails({
+    placeId: marker.id
+  }, function(place, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+      // Set the marker property on this infowindow so it isn't created again.
+      infowindow.marker = marker;
+      var innerHTML = '<div>';
+      if (place.name) {
+        innerHTML += '<strong>' + place.name + '</strong>';
+      }
+      if (place.formatted_address) {
+        innerHTML += '<br>' + place.formatted_address;
+      }
+      if (place.website) {
+        innerHTML += '<br><a href="' + place.website + '">website</a>'
+      }
+      if (place.formatted_phone_number) {
+        innerHTML += '<br>' + place.formatted_phone_number;
+      }
+      if (place.opening_hours) {
+        innerHTML += '<br><br><strong>Hours:</strong><br>' +
+            place.opening_hours.weekday_text[0] + '<br>' +
+            place.opening_hours.weekday_text[1] + '<br>' +
+            place.opening_hours.weekday_text[2] + '<br>' +
+            place.opening_hours.weekday_text[3] + '<br>' +
+            place.opening_hours.weekday_text[4] + '<br>' +
+            place.opening_hours.weekday_text[5] + '<br>' +
+            place.opening_hours.weekday_text[6];
+      }
+      if (place.photos) {
+        innerHTML += '<br><br><img src="' + place.photos[0].getUrl(
+            {maxHeight: 100, maxWidth: 200}) + '">';
+      }
+      innerHTML += '</div>';
+      infowindow.setContent(innerHTML);
+      infowindow.open(map, marker);
+      // Make sure the marker property is cleared if the infowindow is closed.
+      infowindow.addListener('closeclick', function() {
+        infowindow.marker = null;
+      });
+    }
+  });
 }
